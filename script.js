730888805877fa479c86fad9f6816e87be8219d5
Edwin Gichuhi <edwingichuhis@gmail.com>
2023-08-08 22:32:11 +0300

    Remove comments from javascript

--- script.js
+++ script.js
@@ -2,54 +2,36 @@ const myBooks = [];
 const addBtn = document.getElementById('add-btn');
 const listBooks = document.getElementById('list-books');
 
-// load books from localStorage
 const storedBooks = localStorage.getItem('myBooks');
 if (storedBooks) {
-  myBooks.push(...JSON.parse(storedBooks)); // Parse the storedBooks JSON string
+  myBooks.push(...JSON.parse(storedBooks));
 }
 
 function showBooks() {
   listBooks.innerHTML = '';
 
-  // Show all books in the myBooks array using forEach() loop
   myBooks.forEach((myBook) => {
-    const myBookDiv = document.createElement('div'); // Create a new div for each book
+    const myBookDiv = document.createElement('div');
     myBookDiv.classList.add('row');
 
-    // Create h4 element for title and append textContent
-    const bookTitle = document.createElement('h4');
-    bookTitle.textContent = myBook.bookTitle;
-
-    // Create h4 element for author and append textContent
     const bookDetails = document.createElement('h4');
     bookDetails.textContent = `${myBook.bookTitle} by ${myBook.bookAuthor}`;
 
-    // Create remove button
     const removeBtn = document.createElement('button');
     removeBtn.classList.add('remove-btn');
     removeBtn.textContent = 'Remove';
-
-    // Add event listener to remove button
     removeBtn.addEventListener('click', () => {
       // eslint-disable-next-line no-use-before-define
       removeBook(myBook.bookTitle);
     });
 
-    // Add hr
-    // const hr = document.createElement('hr');
-
-    // Append items to myBookDiv
     myBookDiv.appendChild(bookDetails);
-    // myBookDiv.appendChild(bookAuthor);
     myBookDiv.appendChild(removeBtn);
-    // myBookDiv.appendChild(hr);
-
-    listBooks.appendChild(myBookDiv); // Append myBookDiv to the listBooks container
+    listBooks.appendChild(myBookDiv);
   });
 }
 
 function addBook() {
-  // Pick title and author values and push to myBooks({title, author})
   const bookTitle = document.getElementById('title').value.trim();
   const bookAuthor = document.getElementById('author').value.trim();
   const errorMsg = 'Both Title and Author must be filled out!';
@@ -60,10 +42,7 @@ function addBook() {
     return;
   }
 
-  // Save to myBooks array
   myBooks.push({ bookTitle, bookAuthor });
-
-  // Save updated myBooks array to localStorage
   localStorage.setItem('myBooks', JSON.stringify(myBooks));
 
   // Call showBooks() to see all books
@@ -75,10 +54,7 @@ function addBook() {
 }
 
 function removeBook(bookTitleToRemove) {
-  // Use filter to create a new array without the book to remove
   const myBooksUpdated = myBooks.filter((myBook) => myBook.bookTitle !== bookTitleToRemove);
-
-  // Save updated myBooks array to localStorage
   localStorage.setItem('myBooks', JSON.stringify(myBooksUpdated));
 
   // Update the myBooks array with the updated version
@@ -89,7 +65,6 @@ function removeBook(bookTitleToRemove) {
   showBooks();
 }
 
-// Add event listeners
 addBtn.addEventListener('click', addBook);
 
 // Initial display of books
